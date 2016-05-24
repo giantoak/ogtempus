fc0f4f31bef8fe5ddf2605658f31868913934527
pmlandwehr <pmlandwehr@gmail.com>
2016-05-24 12:10:57 -0400

    Add URL and R string formatters

--- opencpu_example.py
+++ opencpu_example.py
@@ -7,6 +7,47 @@ import tangelo
 opencpu_url = 'https://public.opencpu.org/ocpu'
 
 
+def _url_fmt(*args):
+    """
+    Joins given arguments into a url, stripping trailing slashes.
+    """
+    return '/'.join(map(lambda x: str(x).rstrip('/'), args))
+
+
+def _r_list_fmt(x):
+    """
+
+    :param x: Some variable
+    :returns: `str` -- variable formatted as r list
+    """
+    if isinstance(x, (list, set)):
+        return 'c({})'.format(str(x)[1:-2])
+    return 'c({})'.format(x)
+
+
+def _r_ts_fmt(x, frequency=None):
+    """
+
+    :param x:
+    :param frequency:
+    :returns: `str` -- variable formatted as r time series
+    """
+    if frequency is None:
+        return 'ts({})'.format(_r_list_fmt(x))
+
+    return 'ts({}, frequency={})'.format(_r_list_fmt(x), frequency)
+
+
+def _r_array_fmt(x, dim_one, dim_two):
+    """
+
+    :param x:
+    :param int dim_one:
+    :param int dim_two:
+    :returns: `str` -- variables formatted as r array
+    """
+    return 'array({}, dim={})'.format(_r_list_fmt(x), _r_list_fmt([dim_one, dim_two]))
+
 def get_time_series(date, value):
     """
     :param date:
